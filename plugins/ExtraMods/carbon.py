795709d0242da3eb83811048e50836031120d42d
Madflix Official <143621473+jishusinha@users.noreply.github.com>
2023-10-05 12:48:13 +0530

    Update carbon.py

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -21,17 +21,17 @@ async def make_carbon(code, tele=False):
 @Client.on_message(filters.command("carbon"))
 async def carbon_func(b, message):
     if not message.reply_to_message:
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
+        return await message.reply_text("Rᴇᴘʟʏ Tᴏ A Tᴇxᴛ Mᴇssᴀɢᴇ Tᴏ Mᴀᴋᴇ Cᴀʀʙᴏɴ.")
     if not message.reply_to_message.text:
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
+        return await message.reply_text("Rᴇᴘʟʏ Tᴏ A Tᴇxᴛ Mᴇssᴀɢᴇ Tᴏ Mᴀᴋᴇ Cᴀʀʙᴏɴ.")
     user_id = message.from_user.id
-    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
+    m = await message.reply_text("Pʀᴏᴄᴇssɪɴɢ....")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
+    await m.edit("Uᴘʟᴏᴀᴅɪɴɢ....")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴍᴀᴅᴇ ʙʏ: @Madflix_Botz**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/Madflix_Botz")]]),                   
+        caption="**Mᴀᴅᴇ Bʏ: @Madflix_Botz**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Sᴜᴩᴩᴏʀᴛ Uꜱ", url="https://t.me/Madflix_Botz")]]),                   
     )
     await m.delete()
     carbon.close()
